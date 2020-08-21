cc622694d10064eb6f96c1e98f54332e33f92ad4
zedred79 <52261079+zedred79@users.noreply.github.com>
2020-08-21 14:48:16 +0200

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+echo 'build new image';
 docker build -t apcupsd-cgi .;
+echo 'stop container'
 docker stop apcupsd-cgi;
+echo 'remove container'
 docker rm apcupsd-cgi;
+echo 'create anda start new container'
 echo 'docker run -d --name apcupsd-cgi -p 80 apcupsd-cgi';
 docker run -d --name apcupsd-cgi -p 80 apcupsd-cgi;
 docker port apcupsd-cgi
