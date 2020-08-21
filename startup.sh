ed120baf0aac880938237fbcd00530612b1c3019
zedred79 <52261079+zedred79@users.noreply.github.com>
2020-08-21 14:51:12 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+#print host IP
 hostIp=$(/sbin/ip route|awk '/default/ { print $3 }');
 
+#change the IP to monitor with the host IP in /etc/apcupsd/hosts.conf
 sed -i "s/MONITOR 127.0.0.1/MONITOR $hostIp/g" /etc/apcupsd/hosts.conf
+
+#start fcgiwarap
 service fcgiwrap start;
