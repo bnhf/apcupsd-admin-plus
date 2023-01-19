6639ac833a716b2509233472046f2ce2ddbca4b8
bnhf <41088895+bnhf@users.noreply.github.com>
2023-01-19 11:02:05 -0700

    Remove all <CR> and add env vars

--- startup.sh
+++ startup.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-rm /etc/apcupsd/apcupsd.conf && touch /etc/apcupsd/apcupsd.conf
-array=( "$UPSHOSTS" )
+# remove sample and create empty hosts file
+rm /etc/apcupsd/hosts.conf && touch /etc/apcupsd/hosts.conf
 
-for i in "${array[@]}"
-    do 
-        echo "MONITOR $i " >> /etc/apcupsd/apcupsd.conf
+# populate two arrays with host and UPS names
+HOSTS=( $UPSHOSTS )
+NAMES=( $UPSNAMES )
+
+# add monitors to hosts.conf for each host and UPS name combo
+for ((i=0;i<${#HOSTS[@]};i++))
+    do
+        echo "MONITOR ${HOSTS[$i]} \"${NAMES[$i]}\"" >> /etc/apcupsd/hosts.conf
+        echo "MONITOR ${HOSTS[$i]} \"${NAMES[$i]}\""
+done
         
 # print host IP
 # hostIp=$(/sbin/ip route|awk '/default/ { print $3 }')
@@ -16,4 +23,3 @@ for i in "${array[@]}"
 # start fcgiwarap
 /etc/init.d/fcgiwrap start
 nginx -g 'daemon off;'
-
